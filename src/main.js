4b2ea2b39349386a88911ece5a154f730e516b7d
guhungyin <hieosi840101@gmail.com>
2024-09-03 01:15:30 +0800

    暫時不導入 sweetalert2

--- src/main.js
+++ src/main.js
@@ -6,10 +6,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './assets/all.scss'
-import Swal from 'sweetalert2'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(Swal)
 app.mount('#app')
